a9e19b1d355735948e26f767366de7e7e1ae6522
Dipali <Dipali6091@conestogac.on.ca>
2023-06-09 14:09:15 -0400

    Isosceles Triangle Test case

--- Assignment2/Class1.cs
+++ Assignment2/Class1.cs
@@ -25,7 +25,59 @@ namespace Assignment2
             Assert.AreEqual(expected, result);
         }
 
-       
+        [Test]
+        public void ValidIsoscelesTriangle_Input5and5and6_OutputValidIsoscelesTrianle()
+        {
+            // Arrange
+            int firstSide = 5;
+            int secondSide = 5;
+            int thirdSide = 6;
+
+            string expected = "The triangle is valid and is an ISOSCELES";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ValidIsoscelesTraiangle_Input1and5and5_OutputValidIsoscelesTriangle()
+        {
+            // Arrange
+            int firstSide = 1;
+            int secondSide = 5;
+            int thirdSide = 5;
+
+            string expected = "The triangle is valid and is an ISOSCELES";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ValidIsoscelesTriangle_Input6and3and3_OutputValidIsoscelesTriangle()
+        {
+            // Arrange
+            int firstSide = 6;
+            int secondSide = 3;
+            int thirdSide = 6;
+
+            string expected = "The triangle is valid and is an ISOSCELES";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+
 
 
 
