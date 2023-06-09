7f33bac609667505e7068c39f7a1e6167998d358
Dipali <Dipali6091@conestogac.on.ca>
2023-06-09 13:59:20 -0400

    First Commit - Test case for Equilateral Triangle

---
+++ Assignment2/Class1.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TriangleSolver;
+namespace Assignment2
+{
+
+    [TestFixture]
+    public class Class1
+    {
+
+        [Test]
+        public void ValidEquilateralTriangle_Input6and6and6_OutputvalidEquilateralTriangle()
+        {
+            // Arrange
+            int firstSide = 6;
+            int secondSide = 6;
+            int thirdSide = 6;
+
+            string expected = "The triangle is valid and is an EQUILATERAL";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+       
+
+
+
+    }
+}
